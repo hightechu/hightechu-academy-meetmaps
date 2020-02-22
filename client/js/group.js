e1b44614bae5428c512367739a322ef6c0701785
MatthewHightech <Smoose007@gmail.com>
2020-02-21 16:08:49 -0800

    createGroup (incomplete)

---
+++ client/js/group.js
@@ -0,0 +1,25 @@
+// creates a new group in the group model
+function createGroup() {
+    // If the response has an error -> inform user
+    // Else is successful -> inform user
+    responseStatus = function (response, status) {
+        if ('error' in response) {
+        // Inform User of Error
+        console.log("{0}: {1}".format(status, response.error.message));
+        }
+        else {
+        // Group created Successfully
+        console.log("Group Created!");
+        // Redirect User to Homepage
+        window.location = "/";
+        }
+    }
+
+    // Grab data index page
+    var jsonObj = new Object();
+    jsonObj.name = document.getElementById("groupName").value;
+    jsonObj.defLocation = document.getElementById("defLocation").value;
+
+    // Connect to the API
+    connectAPI("groups", "POST", responseStatus, jsonObj);
+}
