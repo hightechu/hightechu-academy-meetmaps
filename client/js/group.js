2cf112ceff2e935cce4afecac76d444818e0f014
MatthewHightech <Smoose007@gmail.com>
2020-02-22 10:14:58 -0800

    addUserToGroup

--- client/js/group.js
+++ client/js/group.js
@@ -22,4 +22,27 @@ function createGroup() {
 
     // Connect to the API
     connectAPI("groups", "POST", responseStatus, jsonObj);
+    addUserToGroup(); 
+}
+
+function addUserToGroup() {
+        // If the response has an error -> inform user
+    // Else is successful -> inform user
+    responseStatus = function (response, status) {
+        if ('error' in response) {
+        // Inform User of Error
+        console.log("{0}: {1}".format(status, response.error.message));
+        }
+        else {
+        // Group created Successfully
+        console.log("Member added!");
+        }
+    }
+    connectAPI("users/{0}".format(getCookie("userId")), "GET", responseStatus);
+    // Grab data index page
+    var jsonObj = new Object();
+    jsonObj.user = response.email;
+    // Connect to the API
+    connectAPI("groups", "POST", responseStatus, jsonObj);
+
 }
