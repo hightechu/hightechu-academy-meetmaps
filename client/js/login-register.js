c5990f5b330521793004033be2740cbd6f6a7f7f
MatthewHightech <Smoose007@gmail.com>
2020-02-24 17:44:45 -0800

    fixed silly typo

--- client/js/login-register.js
+++ client/js/login-register.js
@@ -59,6 +59,6 @@ function login() {
 
 function signOut() {
     document.cookie = "userAuth=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    document.cookie = "userID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "userId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location = "/index.html"; 
 }
