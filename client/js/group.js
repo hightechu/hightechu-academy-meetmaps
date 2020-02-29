f3062ae922b770bd0a5398a4fee5f32eca35a08f
MatthewHightech <Smoose007@gmail.com>
2020-02-29 15:24:38 -0800

    showGroupsUserIsIn and Buggy inGroup

--- client/js/group.js
+++ client/js/group.js
@@ -54,29 +54,46 @@ function addUserToGroup(userAdd, groupAdd) {
 } // addUserToGroup
 
 function showGroups() {
-    responseStatus = function (response, status) {
+    var currentUser = getCookie("userId");
+    response = function(response, status) {
         if ('error' in response) {
-            // Inform User of Error
             console.log("{0}: {1}".format(status, response.error.message));
-        }
-        else {
-          console.log(response.id, response.userID, response.groupID); 
-            // Group created Successfully
-            /*
-            var group = document.createElement("BUTTON");  //<button> element
-            var t = document.createTextNode("Edit"); // Create a text node
-            btn.appendChild(t);   
-            btn.onclick = function(){inGroup(response.groupID)};
-            btn.className = "groupButton";
-            btn.setAttribute("id", "edit");
-            groups.appendChild(btn);//to show on myView
-            */
-        }
-    }
+        } else {
+            for (var i = 1; i <= response.length; i++) {
+                var group = document.createElement("BUTTON");  //<button> element
+                var t = document.createTextNode(response[i-1].groupID); // Create a text node
+                group.appendChild(t); 
+                //group.onclick = function(){inGroup(response[i].groupID)};
+                group.addEventListener('click', function(){ inGroup(this.id)});
+                group.className = "groupButton";
+                group.setAttribute("id", "groupName" + i);
+                groups.appendChild(group);//to show on myView 
+            } //for each group
+        } // no error with group
+    } // response
+    connectAPI("groupMembers?filter[userID]={0}".format(currentUser), "GET", response); 
     // Connect to the API
-    connectAPI("groupMembers", "GET", responseStatus);
 }
 
-function inGroup(groupID) {
-
-}
+function inGroup(id) {
+    id = id.substring(9, id.length+1); 
+    response = function(response, status) {
+        if ('error' in response) {
+            console.log("{0}: {1}".format(status, response.error.message));
+        } else {
+            showUsers = function(users, status) {
+                if ('error' in users) {
+                    console.log("{0}: {1}".format(status, users.error.message));
+                } else {
+                    for (var i = 0; i < users.length; i++) {
+                        console.log(users[i].userID); 
+                    }
+                }
+            } // showUsers
+            console.log("Group: " + response.groupID); // 30
+            connectAPI("groupMembers?filter[groupID]={0}".format(response.groupID), "GET", showUsers); 
+        } // no error with group
+    } // response
+    //console.log(id); 
+    connectAPI("groupMembers/{0}".format(id), "GET", response); 
+} // inGroup
