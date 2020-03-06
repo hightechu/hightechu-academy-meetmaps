c8f356c8257633de416618f3dd7b7b452c145b40
Matt Smith <47284433+MatthewHightech@users.noreply.github.com>
2020-03-06 15:33:06 -0800

    Matt smith 02 25 (#27)
    * Finished Server and newGroup and addUserToGroup ready for testing/debugging
    
    * New Group (Working) AddUserToGroup (Working)
    
    * signOut not working
    
    * createGroup UserID instantiation fix (changed from server req to getcookie)
    
    * signOut button fixed (now deletes cookies)
    
    * deleting commented code
    
    * fixed silly typo
    
    * showGroupsUserIsIn and Buggy inGroup
    
    * working "users in group
    
    * working group interaction upto display users
    
    * add user to group + new model with public usernames
    
    * shows all users in a group
    
    * saves users location and updates on reload
    
    * each time a group is selected, the allMembersPos array gets filled with each members lat and lng
    
    * removed node from package.json

--- client/js/group.js
+++ client/js/group.js
@@ -1,3 +1,4 @@
+
 // creates a new group in the group model
 function createGroup() {
     var userID;
@@ -7,15 +8,15 @@ function createGroup() {
     newGroup = function (response, status) {
         if ('error' in response) {
         // Inform User of Error
-        console.log("{0}: {1}".format(status, response.error.message));
+            console.log("{0}: {1}".format(status, response.error.message));
         }
         else {
-        // Group created Successfully
-        console.log("Group Created!");
-        userID = getCookie("userId");
-        groupID = "{0}".format(response.id);
-        console.log(userID + " - " + groupID);
-        addUserToGroup(userID, groupID); 
+            // Group created Successfully
+            console.log("Group Created!");
+            userID = getCookie("userId");
+            groupID = "{0}".format(response.id);
+            console.log(userID + " - " + groupID);
+            addUserToGroup(userID, groupID); 
         }
     }
 
@@ -66,34 +67,62 @@ function showGroups() {
                 //group.onclick = function(){inGroup(response[i].groupID)};
                 group.addEventListener('click', function(){ inGroup(this.id)});
                 group.className = "groupButton";
-                group.setAttribute("id", "groupName" + i);
+                group.setAttribute("id", "groupName" + response[i-1].groupID);
                 groups.appendChild(group);//to show on myView 
             } //for each group
         } // no error with group
     } // response
-    connectAPI("groupMembers?filter[userID]={0}".format(currentUser), "GET", response); 
-    // Connect to the API
+    connectAPI('groupMembers?filter={"where":{"userID":"' + "{0}".format(currentUser) + '"}}', "GET", response); 
 }
 
 function inGroup(id) {
-    id = id.substring(9, id.length+1); 
-    response = function(response, status) {
-        if ('error' in response) {
-            console.log("{0}: {1}".format(status, response.error.message));
+    showUsers = function(users, status) {
+        if ('error' in users) {
+            console.log("{0}: {1}".format(status, users.error.message));
         } else {
-            showUsers = function(users, status) {
-                if ('error' in users) {
-                    console.log("{0}: {1}".format(status, users.error.message));
+            groupName = function(group, status) {
+                if ('error' in group) {
+                    console.log("{0}: {1}".format(status, group.error.message));
                 } else {
-                    for (var i = 0; i < users.length; i++) {
-                        console.log(users[i].userID); 
-                    }
-                }
-            } // showUsers
-            console.log("Group: " + response.groupID); // 30
-            connectAPI("groupMembers?filter[groupID]={0}".format(response.groupID), "GET", showUsers); 
-        } // no error with group
-    } // response
-    //console.log(id); 
-    connectAPI("groupMembers/{0}".format(id), "GET", response); 
+                    console.log(group.name); 
+                    if (document.getElementById("groupTitle")) {
+                        document.getElementById("groupTitle").innerHTML = group.name; 
+                    } else {
+                        var gHeader = document.createElement("h2");
+                        var t = document.createTextNode(group.name); 
+                        gHeader.appendChild(t); 
+                        gHeader.setAttribute("id", "groupTitle");
+                        groups.appendChild(gHeader); 
+                    } // if element exists
+                } // if error
+            } // groupName
+            connectAPI("groups/{0}".format(id), "GET", groupName);
+            if (document.getElementsByClassName("groupUsers")) {
+                $('.groupUsers').remove();
+            }
+            for (var i = 0; i < users.length; i++) {
+                usersInGroup = function(uname, status) {
+                    if ('error' in uname) {
+                        console.log("{0}: {1}".format(status, uname[0].error.message));
+                    } else {
+                        console.log(uname[0].name); 
+                        var gHeader = document.createElement("h4");
+                        var t = document.createTextNode("- " + uname[0].name); 
+                        gHeader.appendChild(t); 
+                        gHeader.className = "groupUsers";
+                        gHeader.setAttribute("id", "userTitle" + i);
+                        groups.appendChild(gHeader); 
+                    } // if error
+                } // usersInGroup
+                console.log(users[i].userID); 
+                connectAPI('usernames?filter={"where":{"useridnty":"' + users[i].userID + '"}}', "GET", usersInGroup);
+            } // for
+        } // else - no error
+    } // showUsers
+    id = id.substring(9, id.length+1); 
+    console.log("Group: " + id); 
+    currentGroupId = id; 
+    connectAPI('groupMembers?filter={"where":{"groupID":"' + id + '"}}', "GET", showUsers);
+    document.getElementById("invite").style.display = "block";  
+    getMembersLocations(id); 
 } // inGroup
