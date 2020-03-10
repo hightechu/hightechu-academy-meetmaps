3af6722558b438b4a2ee68a5a5925b78d15b322c
Trogoatdyte <60553800+Trogoatdyte@users.noreply.github.com>
2020-03-10 13:29:33 -0700

    Bootstrap for main page and login (#28)
    * Bootstrap for main page and login
    
    * Modified boxes and buttons
    
    Co-authored-by: Dominique Charlebois <domo.charlebois@gmail.com>

--- client/js/main.js
+++ client/js/main.js
@@ -3,11 +3,11 @@ var currentGroupId;
 // Add JS Here
 function loadPage () {
     var group = document.getElementById("g");
-    var loginSignup = document.getElementById("loginSignup");
+    //var loginSignup = document.getElementById("loginSignup");
     var button1 = document.getElementById("btn1"); 
     var button2 = document.getElementById("btn2"); 
     if (getCookie("userAuth")) {
-        loginSignup.style.display = "none";
+        document.getElementById("loginSignup").style.display = "none";
         group.style.display = "block"; 
         button1.style.display = "inline-block"; 
         button2.style.display = "inline-block"; 
@@ -16,6 +16,7 @@ function loadPage () {
         group.style.display = "none";
         button1.style.display = "none"; 
         button2.style.display = "none"; 
+        document.getElementById("main").style.display = "none";
     } 
     //deleteLoc(); 
     showGroups();
