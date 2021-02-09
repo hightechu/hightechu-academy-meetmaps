3f8f3189cda1d5e8dd4a431edd5465af6ef5a80d
Malaert64 <48174721+Malaert64@users.noreply.github.com>
2021-02-08 17:12:34 -0800

    Added component navigation for inbox and groups, and put together some basic front-end styling. (#59)

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDataService } from 'src/app/services/user-data.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuPage implements OnInit {
 
-  constructor() { }
+  constructor(public UserDataService: UserDataService, public AuthService: AuthService) { }
 
-  ngOnInit() {
+  page: string = "groupList";
+
+  ngOnInit() {}
+
+  changePage(page: string) {
+    this.page = page;
   }
 
 }
