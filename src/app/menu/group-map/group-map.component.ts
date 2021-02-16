8560b9243f1a9174b24715f19679cb8dde421ba2
Matt Smith <47284433+MatthewHightech@users.noreply.github.com>
2021-02-16 15:20:38 -0800

    Maps Component Init (#81)

--- src/app/menu/group-map/group-map.component.ts
+++ src/app/menu/group-map/group-map.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
+import { MapsService } from 'src/app/services/maps.service';
 import { UserDataService } from 'src/app/services/user-data.service';
 import { InvitePopupComponent } from './invite-popup/invite-popup.component';
 import { Loader } from "@googlemaps/js-api-loader"
+
 
 @Component({
   selector: 'app-group-map',
@@ -13,9 +15,12 @@ export class GroupMapComponent implements OnInit {
 
   invitePopup = null;
 
-  constructor(public userDataService: UserDataService, public popoverController: PopoverController) {}
+  constructor(public userDataService: UserDataService, public popoverController: PopoverController, public mapService: MapsService) {}
 
-  ngOnInit() {}
+
+  ngOnInit() {
+
+  }
 
   popop = async function presentPopover(type: string) {
 
